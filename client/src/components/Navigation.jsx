41e4db320e76017a88928e832185d94952a71294
Sameer Shaik <sameer.kattubadi@gmail.com>
2025-06-25 14:26:12 +0530

    initialize client application with React and Vite

---
+++ client/src/components/Navigation.jsx
@@ -0,0 +1,27 @@
+import "../styles/navigation.css";
+function Navigation() {
+  return (
+    <nav className="glass-navbar">
+      <div className="logo">DeHarvest</div>
+      <ul className="nav-menu">
+        <li>
+          <a href="#home">Home</a>
+        </li>
+        <li>
+          <a href="#features">Features</a>
+        </li>
+        <li>
+          <a href="#staking">Staking</a>
+        </li>
+        <li>
+          <a href="#governance">Governance</a>
+        </li>
+        <li>
+          <a href="#marketplace">Marketplace</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navigation;
