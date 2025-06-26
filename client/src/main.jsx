4a9d3af5e35eda9c93b3631f5518e52a9742ebf5
Sameer Shaik <sameer.kattubadi@gmail.com>
2025-06-27 01:11:34 +0530

    feat: add HeroUIProvider and new dependencies
    - Added @heroui/react and framer-motion to dependencies in package.json.
    - Wrapped the App component with HeroUIProvider in main.jsx for context.
    - Updated index.css to include a new line for better formatting.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/index.css";
 import App from "./page/App";
+import { HeroUIProvider } from "@heroui/react";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <HeroUIProvider>
+      <App />
+    </HeroUIProvider>
   </StrictMode>
 );
