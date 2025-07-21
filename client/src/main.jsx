02f4800e6e91b13e3caa783fae9d42bc3000ba4b
SSameer20 <sameer.kattubadi@gmail.com>
2025-07-21 19:46:18 +0530

    add routes to the project

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,18 @@ import { createRoot } from "react-dom/client";
 import "./styles/index.css";
 import App from "./page/App";
 import Context from "./utils/context/Context";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Airdrop from "./page/Airdrop";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Context>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<App />} />
+          <Route path="/airdrop" element={<Airdrop />} />
+        </Routes>
+      </BrowserRouter>
     </Context>
   </StrictMode>
 );
